45ff430f8821a49f1cf4742c7ff55761fef25d9b
Mateus Otavio <maiamateusotavio@gmail.com>
2021-01-30 17:53:59 -0300

    Insercao de usuario com POST

--- src/main/java/com/maiamateus/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/maiamateus/workshopmongodb/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.maiamateus.workshopmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.maiamateus.workshopmongodb.domain.User;
 import com.maiamateus.workshopmongodb.dto.UserDTO;
@@ -32,5 +36,12 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User obj = service.findaById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		URI uri =ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
