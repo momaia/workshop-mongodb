73fd6689943436a1293af438eed5c574a0a14833
Mateus Otavio <maiamateusotavio@gmail.com>
2021-01-30 17:08:45 -0300

    Obtendo um usuario por id

--- src/main/java/com/maiamateus/workshopmongodb/services/UserService.java
+++ src/main/java/com/maiamateus/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.maiamateus.workshopmongodb.domain.User;
 import com.maiamateus.workshopmongodb.repository.UserRepository;
+import com.maiamateus.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +17,13 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findaById(String id) {
+		User user = repo.findById(id).orElse(null);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 }
