74e13a882ac6e84f27e345082c7e81998ff005fe
Mateus Otavio <maiamateusotavio@gmail.com>
2021-01-31 11:57:31 -0300

    Referenciando os posts do usuario

--- src/main/java/com/maiamateus/workshopmongodb/domain/User.java
+++ src/main/java/com/maiamateus/workshopmongodb/domain/User.java
@@ -1,8 +1,11 @@
 package com.maiamateus.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -18,6 +21,9 @@ public class User implements Serializable{
 	
 	@JsonIgnore
 	private String senha;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -60,6 +66,14 @@ public class User implements Serializable{
 	
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
